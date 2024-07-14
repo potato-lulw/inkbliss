6cc2ae6975e0c6487fcf406e38b461ec0a0c54da
potato-lulw <om.patil17072@gmail.com>
2024-07-15 03:41:41 +0530

    Creating and getting User from convex

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button'
+import { api } from '@/convex/_generated/api'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useConvex, useMutation, useQuery } from 'convex/react'
+import React, { useEffect } from 'react'
+
 
 const DashBoardPage = () => {
+    const { user }: any = useKindeBrowserClient();
+    const convex = useConvex();
+
+    const createUser = useMutation(api.user.createUser);
+
+    useEffect(() => {
+        if (user) {
+            checkUser()
+        }
+    }, [user])
+
+
+    const checkUser = async () => {
+        const result = await convex.query(api.user.getUser, { email: user?.email });
+        if (!result?.length) {
+            createUser({
+                name: user.given_name,
+                email: user.email,
+                image: user.picture
+            }).then((resp) => {
+                console.log(resp)
+            })
+        }
+
+    }
     return (
-        <div>DashBoardPage</div>
+        <div>
+            <Button><LogoutLink>Log out</LogoutLink></Button>
+        </div>
     )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
